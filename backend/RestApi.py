9f70194f97379c30cb6d3ac7ca922b87b6deb13e
Edoardo Tavassi <edoardotavassi232@gmail.com>
2024-05-28 17:23:00 +0200

    Delete backend directory

--- backend/RestApi.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
